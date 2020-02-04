d5bfeb83bdc49e1d311a6f761901b2226e3e8b0f
Thomas Axmann <thomas.a.axmann@fau.de>
2020-02-04 12:47:48 +0100

    Including logging tool

--- dev/main/dummy-main.hpp
+++ dev/main/dummy-main.hpp
@@ -2,10 +2,13 @@
  * this file just exists
  * don't bother to think about it
  */
+#include "config.hpp"
 void dummy_say_hello();
 
 int main()
 {
+    plog::init(plog::debug,"log/dummy.log");
+PLOGN << "Its a wonderful day";
     dummy_say_hello();
     return 0;
 }
--- dev/src/config.hpp
+++ dev/src/config.hpp
@@ -1,8 +1,32 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
+#include <plog/Log.h>
+/* plog is a logging functionality that works like this:
+ *
+ * each main function will have a line saying
+ * ` plog::init(plog::debug, "log/HEADER.log"); `
+ * 
+ * whenever one wants to turn off logging one turns it off by changing that to
+ * ` plog::init(plog::none, "log/HEADER.log"); `
+ *
+ * This works like that:
+ * the plog::none or plog::debug actually set a max severity level that a log
+ * message can have, lower severity levels being ignored and none severity level
+ * leading to the message being printed to the log always.
+ *
+ * Levels are:
+ *  PLOGN << "none";
+ *  PLOGF << "fatal";
+ *  PLOGE << "error";
+ *  PLOGW << "warning";
+ *  PLOGI << "info";
+ *  PLOGD << "debug";
+ *  PLOGV << "verbose";
+ */
+
 /* all typedefs go here
- * were going to optimise for performace by using small ints 
+ * were eg going to optimise for performace by using small ints 
  * etc. and therefore will have several different types
  */
 
