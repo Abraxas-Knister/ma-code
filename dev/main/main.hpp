82da1aa568db51db735209f6d8b2f160411b0d84
abraxas-knister <thoaxm@gmail.com>
2020-03-03 10:02:42 +0100

    wtf this is a lot of work

---
+++ dev/main/main.hpp
@@ -0,0 +1,18 @@
+#include "config.hpp"
+
+int main ()
+{
+// GREENS FUNCTION -> quantum device??
+    // initialise Hamiltonian
+    // initialise state
+    // apply creator
+    // forward evolve
+    // apply annihilator
+    // backwards evolve
+    // make dot product with initial state
+// GET SELF ENERGY
+// GET PARAMETERS (SELF CONSISTENCY)
+// CHECK CONVERGENCE
+// UPDATE HAMILTONIAN AND REDO
+    return 0;
+}
