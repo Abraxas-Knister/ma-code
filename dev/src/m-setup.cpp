8e2c5b5a946e7525cdb3e704e3fb02adeacba39d
abraxas-knister <thoaxm@gmail.com>
2020-03-03 13:54:09 +0100

    Matrix Greensfunction computation (2 site DMFT only!)
    I set up the "m-setup.hpp" header which defines an Setup struct
    this creates and stores internally a 16x16 matrix (from given U and to
    be computed V). In also stores the system state and (it can't yet) it
    can evolve the system state by means of its Hamiltonian.
    
    It can also act by C and Cd operators on the state. (not yet)

---
+++ dev/src/m-setup.cpp
@@ -0,0 +1,48 @@
+#include "m-setup.hpp"
+
+Setup::Setup(const double iniU, const double iniV)
+{
+    this->updateH(iniU, iniV);
+}
+
+int sg(int msk, int orb);
+void Setup::updateH (const double newU, const double newV)
+{
+    U=newU;
+    V=newV;
+    m_operator_t tmp = m_operator_t::Zero();  
+    int j {};
+    for (int i = 0; i<16; i++ )
+    {
+        if (i & 1) tmp(i,i) += -.5*U;
+        if (i & 2) tmp(i,i) += -.5*U;
+        if (i & 1 && i & 2) tmp(i,i) += U;
+        if (!(i & 4) && i & 1)
+        {
+            j = (i | 4) & ~1;
+            tmp(j,i) += V*sg(i,1);
+        }
+        if (!(i & 8) && i & 2)
+        {
+            j = (i | 8) & ~2;
+            tmp(j,i) += V*sg(i,2);
+        }
+        if (!(i & 1) && i & 4)
+        {
+            j = (i | 1) & ~4;
+            tmp(j,i) += V*sg(i,1);
+        }
+        if (!(i & 2) && i & 8)
+        {
+            j = (i | 2) & ~8;
+            tmp(j,i) += V*sg(i,2);
+        }
+    }
+    H.swap(tmp);
+}
+
+int sg(int msk, int orb)
+{
+    bool has = msk & (1<<orb);
+    return 1-2*has;
+}
---
+++ dev/src/m-setup.hpp
@@ -0,0 +1,35 @@
+#ifndef M_SETUP_HPP
+#define M_SETUP_HPP
+#include "config.hpp"
+
+#include <Eigen/Dense>
+
+using m_operator_t = Eigen::Matrix<double,16,16>;
+using m_state_t    = Eigen::Vector<complex,16>;
+
+struct Setup
+{
+    /* Hamiltonian is the one for one site and one bath site ie
+     * site is occupied with  - (Nsu + Nsd) * mu
+     * site has coulomb U(Nsu*Nsd)
+     * site has interaction with bath via V * (CsudCbu + CbudCsu + (u->d))
+     * bath has occupation via e*(Nbu + Nbd)
+     *
+     * N C are annihilation / number operators
+     * Xb Xs are bath or site operators
+     * Xyu Xyd are operators for spin up/down
+     *
+     * Parameters are only V since halve filling sets the parameters mu = U/2
+     * and e=0. (I don't see where this is coming from).
+     */
+    double U,V;
+    m_operator_t H;
+    m_state_t state;
+    Setup(const double iniU, const double iniV);
+    void evolve(const double t);
+    void actWithC(const int orb);
+    void actWithCd(const int orb);
+    void updateH(const double newU, const double newV);
+};
+
+#endif // guard
