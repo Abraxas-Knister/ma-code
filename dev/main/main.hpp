27ded208b63e41dabbff86b40d8c3916321aa2de
abraxas-knister <thoaxm@gmail.com>
2020-04-04 15:20:00 +0200

    rearanged main st it prints to data files

--- dev/main/main.hpp
+++ dev/main/main.hpp
@@ -5,35 +5,32 @@
 
 #include <fstream>
 #include <iomanip>
-#include <iostream>
 #include <vector>
 
 int main ()
 {
-// GREENS FUNCTION -> quantum device??
+    // GREENS FUNCTION -> quantum device??
     double U=1.0,V=1e4;
     double oldV=V*10;
     Setup s(U,V);
     green G(s);
     G.compute();
-// GET SELF ENERGY
-// GET PARAMETERS (SELF CONSISTENCY)
-// CHECK CONVERGENCE
-// UPDATE HAMILTONIAN AND REDO
+
     std::vector<double> vv;
-    while (!areNear(V,oldV,1e-5))
+    while (!areNear(V,oldV,1e-6)) // CHECK CONVERGENCE
     {
         oldV = V;
 
-        G.ckparams(U,V);
-        s.set(U,V);
+        G.ckparams(U,V); // GET SELF ENERGY GET PARAMETERS (SELF CONSISTENCY)
+        s.set(U,V); // UPDATE HAMILTONIAN AND REDO
         G.compute();
 
         vv.push_back(V);
     }
-    std::cout << std::setprecision(15);
-    std::cout << G;
-    std::cerr << std::setprecision(15);
-    for (auto i:vv) std::cerr << i <<'\n';
+    std::ofstream fG("data/mat-gf");
+    fG << std::setprecision(15) << G;
+    std::ofstream vvec("data/mat-converg");
+    vvec << std::setprecision(15);
+    for (auto i:vv) vvec << i <<'\n';
     return 0;
 }
